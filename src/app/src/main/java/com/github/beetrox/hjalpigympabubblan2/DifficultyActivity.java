08258bf400d1da69b74f71ff05dda80204491d79
Tora Lindberg <toto0181@hotmail.com>
2018-11-14 09:20:19 +0100

    added working difficulty counter

--- src/app/src/main/java/com/github/beetrox/hjalpigympabubblan2/DifficultyActivity.java
+++ src/app/src/main/java/com/github/beetrox/hjalpigympabubblan2/DifficultyActivity.java
@@ -20,6 +20,9 @@ public class DifficultyActivity extends AppCompatActivity {
     Spinner secondSkillSpinner;
     Spinner thirdSkillSpinner;
 
+    List<Skill> skills = new ArrayList<>();
+    List<String> skillNames = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,32 +33,58 @@ public class DifficultyActivity extends AppCompatActivity {
         secondSkillSpinner = findViewById(R.id.secondSkillSpinner);
         thirdSkillSpinner = findViewById(R.id.thirdSkillSpinner);
 
-//        List<com.github.beetrox.hjalpigympabubblan2.Skill> skillsList =  new ArrayList<com.github.beetrox.hjalpigympabubblan2.Skill>();
-//        skillsList.add("Rondat");
-//        skillsList.add("Flickis");
-//        skillsList.add("Salto grupperad");
-//        skillsList.add("Salto pik");
+        PopulateSkills();
+        PopulateSkillNames();
 
-        List<Double> skillArray =  new ArrayList<Double>();
-        skillArray.add(0.0);
-        skillArray.add(0.1);
-        skillArray.add(0.2);
-        skillArray.add(0.3);
+//        skillNames =  new ArrayList<>();
+//        skillNames.add("Välj övning");
+//        skillNames.add(rondat.getName());
+//        skillNames.add(flickis.getName());
+//        skillNames.add(saltoGrupperad.getName());
+//
+        ArrayAdapter<String> skillsAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, skillNames);
+//
+        skillsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        firstSkillSpinner.setAdapter(skillsAdapter);
+        secondSkillSpinner.setAdapter(skillsAdapter);
+        thirdSkillSpinner.setAdapter(skillsAdapter);
 
-        ArrayAdapter<Double> skillAdapter = new ArrayAdapter<Double>(
-                this, android.R.layout.simple_spinner_item, skillArray);
+//        List<String> skillArray =  new ArrayList<>();
+//        skillArray.add("0.0");
+//        skillArray.add("0.1");
+//        skillArray.add("0.2");
+//        skillArray.add("0.3");
+//
+//        ArrayAdapter<String> skillAdapter = new ArrayAdapter<>(
+//                this, android.R.layout.simple_spinner_item, skillArray);
+//
+//        skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        firstSkillSpinner.setAdapter(skillAdapter);
+//        secondSkillSpinner.setAdapter(skillAdapter);
+//        thirdSkillSpinner.setAdapter(skillAdapter);
 
-        skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        firstSkillSpinner.setAdapter(skillAdapter);
-        secondSkillSpinner.setAdapter(skillAdapter);
-        thirdSkillSpinner.setAdapter(skillAdapter);
+//        List<Double> skillArray =  new ArrayList<Double>();
+//        skillArray.add(0.0);
+//        skillArray.add(0.1);
+//        skillArray.add(0.2);
+//        skillArray.add(0.3);
+//
+//        ArrayAdapter<Double> skillAdapter = new ArrayAdapter<Double>(
+//                this, android.R.layout.simple_spinner_item, skillArray);
+//
+//        skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        firstSkillSpinner.setAdapter(skillAdapter);
+//        secondSkillSpinner.setAdapter(skillAdapter);
+//        thirdSkillSpinner.setAdapter(skillAdapter);
 
         firstSkillSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view,int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                double result = DifficultyResult();
+                System.out.println("first item selected");
+                double result = DifficultyResult(position);
                 difficultyTextView.setText(String.valueOf(result));
             }
 
@@ -68,9 +97,10 @@ public class DifficultyActivity extends AppCompatActivity {
         secondSkillSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view,int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                double result = DifficultyResult();
+                System.out.println("second item selected");
+                double result = DifficultyResult(position);
                 difficultyTextView.setText(String.valueOf(result));
             }
 
@@ -83,9 +113,10 @@ public class DifficultyActivity extends AppCompatActivity {
         thirdSkillSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view,int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                double result = DifficultyResult();
+                System.out.println("third item selected");
+                double result = DifficultyResult(position);
                 difficultyTextView.setText(String.valueOf(result));
             }
 
@@ -96,11 +127,42 @@ public class DifficultyActivity extends AppCompatActivity {
         });
     }
 
-    public double DifficultyResult() {
+    public double DifficultyResult(int position) {
 
         double result;
-        result = Double.valueOf(firstSkillSpinner.getSelectedItem().toString()) + Double.valueOf(secondSkillSpinner.getSelectedItem().toString()) + Double.valueOf(thirdSkillSpinner.getSelectedItem().toString());
+        double difficulty = 0;
+        String skillName = skillNames.get(position);
+
+        for(int i = 0; i < skills.size(); i++) {
+            Skill skill = skills.get(i);
+            if(skill.name.equals(skillName)) {
+                difficulty = skill.getDifficulty();
+            }
+        }
+        result = difficulty;
+                // if no skill use 0.0
+//        result = Double.valueOf(firstSkillSpinner.getSelectedItem().toString()) + Double.valueOf(secondSkillSpinner.getSelectedItem().toString()) + Double.valueOf(thirdSkillSpinner.getSelectedItem().toString());
 //        result = firstSkillSpinner.getSelectedItem();
         return result;
+    }
+
+    public void PopulateSkills() {
+        Skill noneSelected = new Skill("None selected", 0);
+        Skill rondat = new Skill("Rondat", 0.1);
+        Skill flickis = new Skill("Flickis", 0.15);
+        Skill saltoGrupperad = new Skill("Salto grupperad", 0.2);
+
+        skills.add(noneSelected);
+        skills.add(rondat);
+        skills.add(flickis);
+        skills.add(saltoGrupperad);
+    }
+
+    public void PopulateSkillNames() {
+        for (int i = 0; i < skills.size(); i++) {
+            Skill skill = skills.get(i);
+            String name = skill.getName();
+            skillNames.add(name);
+        }
     }
 }
--- src/app/src/main/java/com/github/beetrox/hjalpigympabubblan2/Skill.java
+++ src/app/src/main/java/com/github/beetrox/hjalpigympabubblan2/Skill.java
@@ -3,4 +3,25 @@ package com.github.beetrox.hjalpigympabubblan2;
 public class Skill {
     public String name;
     public double difficulty;
+
+    public Skill(String name, double difficulty) {
+        this.name = name;
+        this.difficulty = difficulty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(double difficulty) {
+        this.difficulty = difficulty;
+    }
 }
